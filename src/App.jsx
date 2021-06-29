61a5fdcdb51a866496ac87c7ee0b0b217915ecf6
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2021-06-29 09:13:06 -0300

    feat: add default page with Chakra

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,73 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Link,
+  Button,
+  Stack,
+  UnorderedList,
+  ListItem,
+  ChakraProvider
+} from '@chakra-ui/react';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Container maxW={'3xl'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+            Example with <br/>
+            <Text as={'span'} color={'#646cff'}>
+              Vite & React 18 & ChakraUI
+            </Text>
+          </Heading>
+
+          <Stack
+            direction={'column'}
+            color="gray.600"
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+              <UnorderedList spacing={5} textAlign="left">
+                <ListItem>💡 Instant Server Start</ListItem>
+                <ListItem>⚡️ Lightning Fast HMR</ListItem>
+                <ListItem>🛠️ Rich Features</ListItem>
+                <ListItem>📦 Optimized Build</ListItem>
+                <ListItem>🔩 Universal Plugin Interface</ListItem>
+                <ListItem>🔑 Fully Typed APIs</ListItem>
+              </UnorderedList>
+          </Stack>
+
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+            <Link href="https://github.com/mauriciomutte/vite-example" isExternal>
+              <Button
+                colorScheme={'#646cff'}
+                bg={'#646cff'}
+                rounded={'full'}
+                px={6}
+              >
+                View Github
+              </Button>
+            </Link>
+          </Stack>
+        </Stack>
+      </Container>
+    </ChakraProvider>
   )
 }
 
